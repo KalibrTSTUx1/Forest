70b42dd74263219a2bf4343e4d6cf93d42525c00
ghimik <124600022+ghimik@users.noreply.github.com>
2023-04-30 18:55:19 +0300

    Add files via upload

---
+++ Forest/ForestTests/ForestTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.Tests
+{
+    [TestClass()]
+    public class ForestTests
+    {
+        [TestMethod()]
+        public void SortArraysTest()
+        {
+            var forest = new Forest();
+            int[] cols = {3, 4, 4 ,3, 6 };
+            int[] rows = {1, 2, 5, 2, 5};
+            int[] exCols = {4, 6, 3, 4, 3};
+            int[] exRows = {5, 5, 2, 2, 1};
+
+            forest.SortArrays(rows, cols);
+
+            if(cols == exCols && rows == exRows)
+            {
+                Assert.AreEqual(1, 1);
+            } else
+            {
+                Assert.Fail();
+            }
+
+        }
+    }
+}
